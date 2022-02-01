582f2fa7fa2128db737bcb2c78bd51563363852c
Luhrel <lucien.perregaux@gmail.com>
2022-02-01 16:05:23 +0100

    Fixeeeeeeeeeeeeeeeeeeeeees

--- docker/image-auditor/src/index.js
+++ docker/image-auditor/src/index.js
@@ -1,13 +1,15 @@
 const protocol = require('./protocol');
-const ll = require('./linked-list');
 const musician = require('./musician');
+const net = require('net');
 
 // We use a standard Node.js module to work with UDP
 const dgram = require('dgram');
 
+const TCP_HOST = '127.0.0.1';
+const TCP_PORT = 2205;
 
 // Active musicians
-const list = new ll.LinkedList();
+let musicians = [];
 
 // Let's create a datagram socket. We will use it to listen for datagrams published in the
 // multicast group by thermometers and containing measures
@@ -17,43 +19,43 @@ s.bind(protocol.port, () => {
     s.addMembership(protocol.address);
 });
 
+// TCP server
+net.createServer((socket) => {
+  socket.write(musicians);
+  socket.close();
+}).listen(TCP_PORT, TCP_HOST);
+
 // This call back is invoked when a new datagram has arrived.
 s.on('message', (msg, source) => {
-  var data = JSON.stringify(msg, null, 2);
+  var data = JSON.parse(msg, null, 2);
 
-  console.log(data);
+  let found = false;
 
   // Update activeSince
-  let found = false;
-  let current = list.head;
-  while(current != null) {
-    if (current.data.uuid = data.uuid) {
-      current.data.activeSince = Date.now();
+  musicians.forEach((item, index, array) => {
+    if (item.uuid = data.uuid) {
+      item.activeSince = Date.now();
       found = true;
-      break;
     }
-    current = current.next;
-  }
-
+  });
   // Insert a new Musician if not in the list
   if (!found) {
-    var instrument = 0;
-    list.insertHead(new musician.Musician(data.uuid, instrument, Date.now));
+    let instrument;
+    for ([key, val] of Object.entries(protocol.instruments))
+    {
+      if (val == data.sound) {
+        instrument = key;
+        break;
+      }
+    }
+    musicians.push(new musician.Musician(data.uuid, instrument, Date.now()));
   }
 });
 
 setInterval(() => {
-  const now = Date.now();
-
-  let current = list.head;
-  let previous;
-  while(current != null) {
-    const ms = now - current.data.time;
-    if (ms > 5000) {
-      previous.next = current.next
-    } else {
-      previous = current;
+  musicians.forEach((item, index, array) => {
+    if (Date.now() - item.activeSince > 5000) {
+      array.splice(index, 1);
     }
-    current = current.next;
-  }
+  });
 }, 100);
